5141209c95aaedd7b3468aa66e7367497039d035
kpbaks <kristoffer.pbs@tuta.io>
2025-04-12 18:27:25 +0200

    build(flake): add packages output to build project with nix

--- flake.nix
+++ flake.nix
@@ -1,4 +1,4 @@
-{
+rec {
   description = "Simulating Multi-agent Path Planning in Complex environments using Gaussian Belief Propagation and Global Path Finding";
 
   inputs = {
@@ -18,27 +18,19 @@
       flake-utils,
       ...
     }@inputs:
+    let
+      cargoToml = with builtins; fromTOML (readFile ./Cargo.toml);
+      inherit (cargoToml.workspace.package) version;
+      name = "magics";
+    in
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import inputs.nixpkgs { inherit system overlays; };
         rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        inherit (pkgs) lib;
 
-        bevy-deps = with pkgs; [
-          udev
-          alsa-lib
-          vulkan-loader
-          xorg.libX11
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXrandr
-          libxkbcommon
-          wayland
-          egl-wayland
-          freetype
-          fontconfig
-        ];
         cargo-subcommands = with pkgs; [
           cargo-bloat
           cargo-expand
@@ -60,47 +52,96 @@
           #   # cargo-profiler
           #   # cargo-feature
         ];
-        rust-deps =
-          with pkgs;
-          [
-            # rustup
-            taplo # TOML formatter and LSP
-            bacon
-            mold # A Modern Linker
-            clang # For linking
-            gdb # debugger
-            # lldb # debugger
-            # rr # time-traveling debugger
-            ra-multiplex
-            graphviz
-            blas
-            # openblas
-            openssl
-            # lapack
-            gcc
-            gfortran
-            zlib
-          ]
-          ++ cargo-subcommands;
         dev-deps = with pkgs; [
+          taplo # TOML formatter and LSP
+          bacon
           just
           typos # spell checker
         ];
       in
-      with pkgs;
       {
-        formatter.${system} = pkgs.alejandra;
-        devShells.default = pkgs.mkShell rec {
-          nativeBuildInputs = [ pkgs.pkg-config ];
-          buildInputs =
-            [
-              rustToolchain
-            ]
-            ++ bevy-deps
-            ++ rust-deps
-            ++ dev-deps;
+        apps.default = {
+          type = "app";
+          program = pkgs.lib.getExe self.packages.${system}.default;
+        };
 
-          LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+        devShells.default = pkgs.mkShell rec {
+          inherit (self.packages.${system}.default) nativeBuildInputs;
+          buildInputs = self.packages.${system}.default.buildInputs ++ [
+            rustToolchain
+          ];
+          packages = cargo-subcommands ++ dev-deps;
+
+          LD_LIBRARY_PATH = lib.makeLibraryPath (buildInputs ++ nativeBuildInputs);
+        };
+
+        formatter.${system} = pkgs.nixpkgs-rfc-style;
+
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = name;
+          inherit version;
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            alsa-lib
+            openssl
+            zlib
+            mold # A Modern Linker
+            clang # For linking
+            wayland
+            egl-wayland
+            libxkbcommon
+          ];
+          buildInputs =
+            with pkgs;
+            [
+              freetype
+              fontconfig
+              vulkan-loader
+              blas
+              # openblas
+              # openssl
+              # lapack
+              gcc
+              gfortran
+              graphviz
+              libxkbcommon
+            ]
+            ++ lib.optionals pkgs.stdenv.isLinux (
+              with pkgs;
+              [
+                (lib.getLib alsa-lib)
+                xorg.libX11
+                xorg.libXcursor
+                xorg.libXi
+                xorg.libXrandr
+                # wayland
+                (lib.getLib wayland)
+                (lib.getLib udev)
+                (lib.getLib libxkbcommon)
+                udev
+                egl-wayland
+              ]
+            );
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            allowBuiltinFetchGit = true;
+          };
+
+          meta = {
+            inherit description;
+            homepage = cargoToml.workspace.package.repository;
+            license = lib.licenses.mit;
+            mainProgram = name;
+          };
+        };
+
+        # overlays.default = final: prev: { ${name} = self.packages.${system}.default; };
+        overlays.default = {
+          ${name} = self.packages.${system}.default;
         };
       }
     );
